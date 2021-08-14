3301b76194fde51777a0b34af8b7c88e16d3eff7
Vagrant User <vagrant@multicloud>
2021-08-14 10:22:26 -0300

    terraform GCP basics

---
+++ providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.64.0"
+    }
+  }
+}
+
+# Configura o Provider Google Cloud com o Projeto
+provider "google" {
+
+  credentials = file("~/gcp.json")
+
+  project = "DEFINIR"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
